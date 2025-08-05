8f01517269ef78baf6cbb78f70c14604568c9d89
Naseer-Ahmadd <naseerrather58@gmail.com>
2025-08-05 19:03:19 +0530

    testing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+    console.log('testiing cidci :', );
   res.send('🚀 Hello from Jenkins CI/CD Pipeline!');
 });
 
